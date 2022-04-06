49634fa7ba3d4c4fd64c542fbc78316ac8b53a61
AliZaid96 <ali.alhussein96@gmail.com>
2022-04-06 04:06:38 +0000

    start to setting rules to the game through if statements

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,23 @@ let compScore = 0;
 
 for (let i = 0; i< imageTags.length; i++){
     imageTags[i].onclick = function(e){
-        console.log(e.target.id);
+
+        let user = e.target.id;
+        let computer = compRandom();
+
+        checkWinner(user, computer);
     }
+}
+
+// computer randomer
+function compRandom() {
+    let choices = ['rock', 'paper', 'scissors'];
+    let randomChoice = Math.floor(Math.random()*3);
+    return choices [randomChoice];
+}
+
+
+// checking the winner function
+function checkWinner(userScore, compScore) {
+    
 }
