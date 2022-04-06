40f98ad5269ca06cf89c690225df010a3586af29
AliZaid96 <ali.alhussein96@gmail.com>
2022-04-06 03:42:09 +0000

    create a function which randoms 3 options

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,8 @@ let imageTags = document.querySelectorAll('img');
 let userScore = 0;
 let compScore = 0;
 
+for (let i = 0; i< imageTags.length; i++){
+    imageTags[i].onclick = function(e){
+        console.log(e.target.id);
+    }
+}
